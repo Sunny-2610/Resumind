4cd2f53ddd8f99f2bc9b47d3aa207b97e083f232
Sunny-2610 <sunnysinha9884@gmail.com>
2025-08-17 12:19:55 +0530

    authentication is done

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,9 @@ import Navbar from "~/Components/Navbar";
 import type { Route } from "./+types/home";
 import { resumes } from "Constants";
 import ResumeCard from "~/Components/ResumeCard";
+import { useEffect } from "react";
+import { usePuterStore } from "~/lib/puter";
+import { useNavigate } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,6 +14,14 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+    const {  auth } = usePuterStore();
+    
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(auth.isAuthenticated) navigate('/auth?next=/');
+    }, [auth.isAuthenticated, ])
+
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
